fe6a4c08ecdc26cbce14ffbda78d52da258bd6a2
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-10-28 14:05:38 -0600

    Added script to shoot player
    Added a new script for an enemy to shoot a player once it gets too close, the script once attached to a gameobject(enemy) needs to recieve a player transform an a projectile game object

---
+++ Assets/Script/ShootPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPlayer : MonoBehaviour {
+    public Transform player;
+    public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(shoot());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator shoot(){
+        while (true) {
+            yield return new WaitForSeconds(1);
+            if (Vector3.Distance(player.position, transform.position) < 10) {
+                Instantiate<GameObject>(projectile);
+                print("Shoot");
+                print(Vector3.Distance(player.position, transform.position));
+            }
+        }
+    }
+}
